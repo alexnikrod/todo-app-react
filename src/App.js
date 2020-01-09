cf5e60fec682d3cb9691d4707ab99abc3b911eb7
alexnikrod <rodkin.alex1@gmail.com>
2020-01-09 14:55:48 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React from 'react';
-import List from './components/List'
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+import { List, AddList, Tasks } from './components';
 
 function App() {
+  const [lists, setLists] = useState(null);
+  const [colors, setColors] = useState(null);
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/lists?_expand=color&_embed=tasks').then(({ data }) => {
+      setLists(data);
+    });
+    axios.get('http://localhost:3001/colors').then(({ data }) => {
+      setColors(data);
+    });
+
+  }, [])
+
+  const onAddList = obj => {
+    const newList = [...lists, obj];  
+    setLists(newList);
+  }
+
   return (
     <div className="todo">
       <div className="todo__sidebar">
@@ -16,26 +36,24 @@ function App() {
               name: 'All Tasks',
             }
         ]} />  
-        <List 
-          items={[
-            {
-              color: 'green',
-              name: 'Purchases',
-            },
-            {
-              color: 'blue',
-              name: 'Frontend',
-              active: true,
-            },
-            {
-              color: 'pink',
-              name: 'Movies and TV Shows',
-            },
-            
-        ]} />  
+        {lists ? (
+          <List
+            items={lists}
+            onRemove={id => {
+              const newLists = lists.filter(item => item.id !==id);
+              setLists(newLists);  
+            }}
+            isRemovable
+          />
+        ) : (
+          'Загрузка...'
+        )}
+        <AddList onAdd={onAddList} colors={colors} />
       </div>
       <div className="todo__tasks">
-
+        {lists && <Tasks list={lists[1]}/>}
+        
+        
       </div>
     </div>
   );
--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,15 +1,37 @@
 import React from 'react';
+import classNames from 'classnames';
+
+import axios from 'axios';
+
+import removeSvg from '../../assets/img/remove.svg';
+
+import Badge from '../Badge';
 
 import './List.scss';
 
-const List = ({ items }) => {
+const List = ({ items, isRemovable, onClick, onRemove }) => {
+
+  const removeList = (item) => {
+    if (window.confirm('Realy delete?')) {
+      axios.delete('http://localhost:3001/lists/' + item.id).then(() => {
+        onRemove(item.id);
+      });
+      
+    }
+  }
 
   return (
-    <ul className="list">
-      {items.map(item => (
-      <li className={item.active ? 'active' : ''}>
-        <i>{item.icon ? item.icon : <i className={`badge badge--${item.color}`}></i>}</i>
+    <ul onClick={onClick} className="list">
+      {items.map((item, index) => (
+      <li key={index} className={classNames(item.className, 
+      { active: item.active })}>
+        <i>{item.icon ? item.icon : <Badge color={item.color.name} />}</i>
         <span>{item.name}</span>
+        {isRemovable && 
+        <img className="list__remove-icon" src={removeSvg} 
+        alt="Remove icon" 
+        onClick={() => removeList(item)}
+        />}
       </li>
       ))}
     </ul>
