1e895e865b1b518a68ec9a2897bbbc80bdb03f23
Alexander Rodkin <43032570+alexnikrod@users.noreply.github.com>
2020-04-21 10:51:41 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,17 +5,6 @@ import App from './App';
 
 import './index.scss';
 
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 4000;
-
-server.use(middlewares);
-server.use(router);
-
-server.listen(port);
-
 ReactDOM.render(
     <Router>
         <App />
